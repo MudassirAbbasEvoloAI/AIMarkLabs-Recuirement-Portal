c2b93b06ced183e47a157b50a874cbf04e7be3f3
MudassirAbbasEvoloAI <Mudassir@goevolo.com>
2026-05-01 06:27:11 +0500

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
         <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 0 220 52" fill="none">
           <circle cx="22" cy="30" r="14" stroke="#13193a" strokeWidth="5.5" fill="none"/>
           <circle cx="29" cy="10" r="5.5" fill="#f47a20"/>
-          <text x="46" y="30" fontFamily="Syne,sans-serif" fontWeight="800" fontSize="22" fill="#13193a">Mark</text>
+          <text x="46" y="30" fontFamily="Syne,sans-serif" fontWeight="800" fontSize="22" fill="#fff">Mark</text>
           <text x="46" y="49" fontFamily="Syne,sans-serif" fontWeight="800" fontSize="18" fill="#f47a20">Labs</text>
         </svg>
       </Link>
